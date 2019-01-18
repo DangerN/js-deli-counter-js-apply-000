01bd3e24c95cd7af8f3b4e57132bd4881627bfd8
convivial-system-4430 <cdneal091@gmail.com>
2019-01-18 19:50:25 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,23 @@
+var katzDeliLine = [];
+ 
+function takeANumber(katzDeliLine,name)
+{
+  var output = "";
+  var linePosition = katzDeliLine.length + 1;
+  
+  katzDeliLine[linePosition - 1] = name;
+  
+  output = "Welcome, " + name + ". You are number " + linePosition + " in line.";
+  
+  return output;
+}
+
+function nowServing(katzDeliLine)
+{
+  
+}
+
+function currentLine(katzDeliLine)
+{
+  
+}
