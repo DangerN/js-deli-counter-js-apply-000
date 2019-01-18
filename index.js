cb168c75e3579d204a55c04603c570a8dd1f00f7
convivial-system-4430 <cdneal091@gmail.com>
2019-01-18 20:56:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ function nowServing(katzDeliLine)
       katzDeliLine[position - 1] = katzDeliLine[position];
       position++;
     }
-    //katzDeliLine[position - 1] = "";
+    //katzDeliLine[position - 1] = ""; worth a try
+    
   }
   else
   {
